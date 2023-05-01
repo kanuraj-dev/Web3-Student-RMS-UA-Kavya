572b602f164600eb259a775fd341cc971794b075
webbywolf <info.webbywolf@gmail.com>
2023-05-01 20:28:01 +0530

    add web3 token

--- frontend/src/lib/shared/upload-ipfs.ts
+++ frontend/src/lib/shared/upload-ipfs.ts
@@ -1,21 +1,22 @@
-import { WEB3STORAGE_TOKEN } from "$env/static/private";
+// import { WEB3STORAGE_TOKEN } from "$env/static/private";
 import { File, Web3Storage } from "web3.storage";
 import { errorSafeFetch } from "./utils";
 
 export const uploadToIPFS = async (ipfsData: Record<string, string>) => {
-	const client = new Web3Storage({
-		token: WEB3STORAGE_TOKEN,
-	});
+  const client = new Web3Storage({
+    token:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDU1QzQ5RGRDM0Q3MzY1RjYzQWY1MDk0RDA3ZTAwNzBkNjVFN2MyOEMiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2ODI5NDg5NTgyNjYsIm5hbWUiOiJXZWIgRGV2bG9wZXIgRGhhcmFtaWsifQ.W8VRHdcFaaBYPxjWnqetbeOEGM6tIqs9JPQ7n6zPmXg",
+  });
 
-	const jsonFile = new File([JSON.stringify(ipfsData)], "data.json", {
-		type: "application/json",
-	});
+  const jsonFile = new File([JSON.stringify(ipfsData)], "data.json", {
+    type: "application/json",
+  });
 
-	const cid = await client.put([jsonFile]);
-	console.log("PINNED: ", cid);
+  const cid = await client.put([jsonFile]);
+  console.log("PINNED: ", cid);
 
-	// used to populate cloudflare cache on pined json data
-	errorSafeFetch(`https://${cid}.ipfs.w3s.link/data.json`);
+  // used to populate cloudflare cache on pined json data
+  errorSafeFetch(`https://${cid}.ipfs.w3s.link/data.json`);
 
-	return cid;
+  return cid;
 };
